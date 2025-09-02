6a7d035493fe8d142fbecc6df275ce9d4e5dcdb5
Bjoern Anters <bjoern.anters@deepl.com>
2025-09-02 12:10:51 +0200

    condition for transform

--- manifests/transform.pp
+++ manifests/transform.pp
@@ -26,7 +26,11 @@ define vector::transform (
   #   },
   # }
 
-  $transform_hash = $parameters + { 'type' => $type, 'inputs' => $inputs, 'condition' => $condition }
+  if $type == 'filter' {
+    $transform_hash = $parameters + { 'type' => $type, 'inputs' => $inputs, 'condition' => $condition }
+  } else {
+    $transform_hash = $parameters + { 'type' => $type, 'inputs' => $inputs }
+  }
 
   $transform_file_name = "${vector::setup::transforms_dir}/${title}.${format}"
 
